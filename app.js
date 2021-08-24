1aeced6f9800f818ccc784016597476dedc985f1
LAZPbanahaker(Andy) <pn0818x@outlook.com>
2021-08-24 16:30:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,5 @@ generateBtn.addEventListener('click', ()=> {
     const min = parseInt(document.getElementById('min').value);
     const result = Math.floor(Math.random() * (max - min) + min);
     const resultHTML = document.getElementById('result');
-    console.log(resultHTML);
     resultHTML.innerHTML = `${result}`
 });
